798bef9093e7cfc87af59d39332ec1c9598489c6
nbtippetts <nbtippetts@gmail.com>
2022-03-31 14:15:28 -0600

    test

--- test/test_ip.py
+++ test/test_ip.py
@@ -3,22 +3,13 @@ import requests
 # url = "http://127.0.0.1:5000/ip/1"
 # url = "http://192.168.0.215:5000/room/1"
 payload = [{
-	'name': 'Climate',
+	'name': 'Light',
 	'IP': '192.168.0.133',
-}, {
-	'name': 'CO2',
-	'IP': '192.168.0.138',
-}, {
-	'name': 'Humidity',
-	'IP': '192.168.0.131',
-}, {
-	'name': 'Exhaust',
-	'IP': '192.168.0.132',
 }]
 count=1
 for p in payload:
 	# url = f"http://127.0.0.1:5000/ip"
-	url = f"http://192.168.1.37:5000/ip"
+	url = f"http://192.168.1.42:5000/ip"
 
 	response = requests.get(url,params=p)
 	print(response.json())
