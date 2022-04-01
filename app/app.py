64db45eccadfaa545ea85a18e723828190ac9d07
nbtippetts <nbtippetts@gmail.com>
2022-04-01 15:43:40 -0600

    test

--- app/app.py
+++ app/app.py
@@ -209,73 +209,73 @@ def is_time_between(begin_time, end_time):
 	else:  # crosses midnight
 		return now_local_dt >= begin_time or now_local_dt <= end_time
 
-def start_task(*args):
-	print(args)
-	logging.info(args)
-	ip_state = IPModel.query.filter_by(ip=args[1]).first()
-	if ip_state.state == False:
-		logs = ClimateScheduleLogModel(name=ip_state.name, start_time=get_local_time(), end_time=get_local_time(), end_time_flag=True, IP=ip_state)
-		db.session.add(logs)
-		db.session.commit()
-		ip_state.state=True
-		db.session.add(ip_state)
-		db.session.commit()
-		return args
-
-def end_task(*args):
-	print(args)
-	logging.info(args)
-	ip_state = IPModel.query.filter_by(ip=args[1]).first()
-	logs = ClimateScheduleLogModel.query.filter_by(IP=ip_state).order_by(ClimateScheduleLogModel.climate_schedule_log_id.desc()).first()
-	if logs.end_time_flag:
-		logs.end_time_flag = False
-		logs.end_time = get_local_time()
-		db.session.add(logs)
-		db.session.commit()
-		ip_state.state = False
-		db.session.add(ip_state)
-		db.session.commit()
-		return args
-
-
 # def start_task(*args):
 # 	print(args)
-# 	url = f'http://{args[1]}/?v={args[0]}'
-# 	res = requests.get(url,timeout=5)
-# 	logging.info(res)
-# 	if res.status_code == 200:
-# 		ip_state = IPModel.query.filter_by(ip=args[1]).first()
-# 		if ip_state.state == False:
-# 			logs = ClimateScheduleLogModel(name=ip_state.name, start_time=get_local_time(), end_time=get_local_time(), end_time_flag=True, IP=ip_state)
-# 			db.session.add(logs)
-# 			db.session.commit()
-# 			ip_state.state=True
-# 			db.session.add(ip_state)
-# 			db.session.commit()
-# 			return res
-# 	else:
-# 		print(res)
-# 		return res
-# 	# print(url)
+# 	logging.info(args)
+# 	ip_state = IPModel.query.filter_by(ip=args[1]).first()
+# 	if ip_state.state == False:
+# 		logs = ClimateScheduleLogModel(name=ip_state.name, start_time=get_local_time(), end_time=get_local_time(), end_time_flag=True, IP=ip_state)
+# 		db.session.add(logs)
+# 		db.session.commit()
+# 		ip_state.state=True
+# 		db.session.add(ip_state)
+# 		db.session.commit()
+# 		return args
+
 # def end_task(*args):
 # 	print(args)
-# 	url = f'http://{args[1]}/?v={args[0]}'
-# 	res = requests.get(url,timeout=5)
-# 	if res.status_code == 200:
-# 		ip_state = IPModel.query.filter_by(ip=args[1]).first()
-# 		logs = ClimateScheduleLogModel.query.filter_by(IP=ip_state).order_by(ClimateScheduleLogModel.climate_schedule_log_id.desc()).first()
-# 		if logs.end_time_flag:
-# 			logs.end_time_flag = False
-# 			logs.end_time = get_local_time()
-# 			db.session.add(logs)
-# 			db.session.commit()
-# 			ip_state.state = False
-# 			db.session.add(ip_state)
-# 			db.session.commit()
-# 			return res
-# 	else:
-# 		print(res)
-# 		return res
+# 	logging.info(args)
+# 	ip_state = IPModel.query.filter_by(ip=args[1]).first()
+# 	logs = ClimateScheduleLogModel.query.filter_by(IP=ip_state).order_by(ClimateScheduleLogModel.climate_schedule_log_id.desc()).first()
+# 	if logs.end_time_flag:
+# 		logs.end_time_flag = False
+# 		logs.end_time = get_local_time()
+# 		db.session.add(logs)
+# 		db.session.commit()
+# 		ip_state.state = False
+# 		db.session.add(ip_state)
+# 		db.session.commit()
+# 		return args
+
+
+def start_task(*args):
+	print(args)
+	url = f'http://{args[1]}/?v={args[0]}'
+	res = requests.get(url,timeout=5)
+	logging.info(res)
+	if res.status_code == 200:
+		ip_state = IPModel.query.filter_by(ip=args[1]).first()
+		if ip_state.state == False:
+			logs = ClimateScheduleLogModel(name=ip_state.name, start_time=get_local_time(), end_time=get_local_time(), end_time_flag=True, IP=ip_state)
+			db.session.add(logs)
+			db.session.commit()
+			ip_state.state=True
+			db.session.add(ip_state)
+			db.session.commit()
+			return res
+	else:
+		print(res)
+		return res
+	# print(url)
+def end_task(*args):
+	print(args)
+	url = f'http://{args[1]}/?v={args[0]}'
+	res = requests.get(url,timeout=5)
+	if res.status_code == 200:
+		ip_state = IPModel.query.filter_by(ip=args[1]).first()
+		logs = ClimateScheduleLogModel.query.filter_by(IP=ip_state).order_by(ClimateScheduleLogModel.climate_schedule_log_id.desc()).first()
+		if logs.end_time_flag:
+			logs.end_time_flag = False
+			logs.end_time = get_local_time()
+			db.session.add(logs)
+			db.session.commit()
+			ip_state.state = False
+			db.session.add(ip_state)
+			db.session.commit()
+			return res
+	else:
+		print(res)
+		return res
 
 def check_ip_state(ip):
 	end_task('high', ip.ip)
@@ -1095,7 +1095,7 @@ class ClimateParameters(Resource):
 		)
 		db.session.add(climate)
 		db.session.commit()
-		if args['climate_start_time'] is not None:
+		if args['climate_start_time'] != '':
 			climate_day_night = ClimateDayNightModel(
 				climate_start_time=args['climate_start_time'],
 				climate_end_time=args['climate_end_time'],
